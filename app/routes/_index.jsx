f03d207d01da8c15043421dcfda24a1dd4bf55fc
JM <jnmrles@gmail.com>
2023-07-19 20:24:00 -0500

    collections section

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,13 +1,71 @@
+import {useLoaderData, Link} from '@remix-run/react';
+import {Image} from '@shopify/hydrogen';
+
 export function meta() {
   return [
     {title: 'Hydrogen'},
     {description: 'A custom storefront powered by Hydrogen'},
   ];
 }
+
+// loaders are functions that get the data required to render the page
+export async function loader({context}) {
+  return await context.storefront.query(COLLECTIONS_QUERY);
+}
+
 export default function Index() {
+  // useLoader is a HOOK gets passed a loaders returned data, the hook takes that data and passes it to your jsx component
+
+  const {collections} = useLoaderData();
+  console.log(collections);
   return (
-    <div>
-      <h3>Hello from the home page!</h3>
-    </div>
+    <section className="w-full gap-4">
+      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-load">
+        Collections
+      </h2>
+      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
+        {collections.nodes.map((collection) => {
+          return (
+            <Link to={`/collections/${collection.handle}`} key={collection.id}>
+              <div className="grid gap-4">
+                {collection?.image && (
+                  <Image
+                    alt={`Image of ${collection.title}`}
+                    data={collection.image}
+                    key={collection.id}
+                    sizes="(max-width: 32em) 100vw, 33vw"
+                    widths={[400, 500, 600, 700, 800, 900]}
+                    loaderOptions={{
+                      scale: 2,
+                      crop: 'center',
+                    }}
+                  />
+                )}
+              </div>
+              <h2>{collection.title}</h2>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
   );
 }
+
+// GRAPHQL QUERY WE ARE PASSING TO OUR LOADER
+const COLLECTIONS_QUERY = `#graphql
+  query FeaturedCollections {
+    collections(first: 3, query: "collection_type:smart") {
+      nodes {
+        id
+        title
+        handle
+        image{
+          altText
+          width
+          height
+          url
+        }
+      }
+    }
+  }
+`;
