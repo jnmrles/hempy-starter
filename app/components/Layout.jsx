187764578acf33ee63ca6f4c8585d13d80e688c6
JM <jnmrles@gmail.com>
2023-07-25 05:08:58 -0500

    hero section, nav section, styling

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -3,6 +3,7 @@ import {Suspense} from 'react';
 import {Await} from '@remix-run/react';
 import {useMatches} from '@remix-run/react';
 import {CartLineItems, CartActions, CartSummary} from '~/components/Cart';
+import Nav from './Nav';
 
 function CartHeader({cart, openDrawer}) {
   return (
@@ -42,23 +43,21 @@ export function Layout({children, title}) {
   const [root] = useMatches();
   const cart = root.data?.cart;
   return (
-    <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
-      <header
+    <div className="flex flex-col min-h-screen antialiased bg-black">
+      <Nav />
+      {/* <header
         role="banner"
         className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-sm`}
       >
         <div className="flex gap-12 w-full items-center">
+          <Nav />
           <a className="font-bold" href="/">
             {title}
           </a>
           <CartHeader cart={cart} openDrawer={openDrawer} />
         </div>
-      </header>
-      <main
-        role="main"
-        id="mainContent"
-        className="flex-grow p-6 md:p-8 lg:p-12"
-      >
+      </header> */}
+      <main role="main" id="mainContent" className="flex-grow  md:p-8 lg:p-0">
         {children}
       </main>
       <Drawer open={isOpen} onClose={closeDrawer}>
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,7 @@
 import {useLoaderData, Link} from '@remix-run/react';
 import {Image} from '@shopify/hydrogen';
 import {motion} from 'framer-motion';
+import Hero from '~/components/Hero';
 
 export function meta() {
   return [
@@ -20,35 +21,74 @@ export default function Index() {
   const {collections} = useLoaderData();
   console.log(collections);
   return (
-    <section className="w-full gap-4">
-      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-load">
-        Collections
-      </h2>
-      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
-        {collections.nodes.map((collection) => {
-          return (
-            <Link to={`/collections/${collection.handle}`} key={collection.id}>
-              <div className="grid gap-4">
-                {collection?.image && (
-                  <Image
-                    alt={`Image of ${collection.title}`}
-                    data={collection.image}
-                    key={collection.id}
-                    sizes="(max-width: 32em) 100vw, 33vw"
-                    widths={[400, 500, 600, 700, 800, 900]}
-                    loaderOptions={{
-                      scale: 2,
-                      crop: 'center',
-                    }}
-                  />
-                )}
-              </div>
-              <h2>{collection.title}</h2>
-            </Link>
-          );
-        })}
+    <>
+      <Hero />
+
+      <div className="relative group">
+        <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+        <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+          <svg
+            className="w-8 h-8 text-purple-600"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"
+            ></path>
+          </svg>
+          <div className="space-y-2">
+            <p className="text-slate-800">
+              Learn how to make a glowing gradient background!
+            </p>
+            <a
+              href="https://braydoncoyer.dev/blog/tailwind-gradients-how-to-make-a-glowing-gradient-background"
+              className="block text-indigo-400"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Read Article →
+            </a>
+          </div>
+        </div>
       </div>
-    </section>
+
+      <section className="w-full gap-4 p-6 text-[#22d3ee]  ">
+        <h2 className="whitespace-pre-wrap max-w-prose font-bold text-load">
+          Collections
+        </h2>
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
+          {collections.nodes.map((collection) => {
+            return (
+              <Link
+                to={`/collections/${collection.handle}`}
+                key={collection.id}
+              >
+                <div className="grid gap-4">
+                  {collection?.image && (
+                    <Image
+                      alt={`Image of ${collection.title}`}
+                      data={collection.image}
+                      key={collection.id}
+                      sizes="(max-width: 32em) 100vw, 33vw"
+                      widths={[400, 500, 600, 700, 800, 900]}
+                      loaderOptions={{
+                        scale: 2,
+                        crop: 'center',
+                      }}
+                    />
+                  )}
+                </div>
+                <h2>{collection.title}</h2>
+              </Link>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 }
 
